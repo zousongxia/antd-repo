87f4256984c18f185b42242775cd33ef4e261b18
邹讼侠 <songxia.zou@hand-china.com>
2024-05-21 14:26:12 +0800

    [ADD]token过期重定向到登录页面

--- src/requestErrorConfig.ts
+++ src/requestErrorConfig.ts
@@ -3,7 +3,6 @@ import type { RequestConfig } from '@umijs/max';
 import { message, notification } from 'antd';
 import { USER_CENTER_TOKEN } from './constant';
 import { history } from '@umijs/max';
-import { stringify } from 'querystring';
 
 // 错误处理方案： 错误类型
 enum ErrorShowType {
@@ -32,6 +31,7 @@ export const errorConfig: RequestConfig = {
   errorConfig: {
     // 错误抛出
     errorThrower: (res) => {
+      console.log('errorThrower', res);
       const { success, data, errorCode, message, showType } = res as unknown as ResponseStructure;
       if (!success) {
         const error: any = new Error(message);
@@ -42,6 +42,7 @@ export const errorConfig: RequestConfig = {
     },
     // 错误接收及处理
     errorHandler: (error: any, opts: any) => {
+      console.log('errorHandler', error);
       if (opts?.skipErrorHandler) throw error;
       // 我们的 errorThrower 抛出的错误。
       if (error.name === 'BizError') {
@@ -75,6 +76,12 @@ export const errorConfig: RequestConfig = {
         // Axios 的错误
         // 请求成功发出且服务器也响应了状态码，但状态代码超出了 2xx 的范围
         message.error(`Response status:${error.response.status}`);
+        // token过期
+        if (error.response.status === 401) {
+          history.replace({
+            pathname: '/user/login',
+          });
+        }
       } else if (error.request) {
         // 请求已经成功发起，但没有收到响应
         // \`error.request\` 在浏览器中是 XMLHttpRequest 的实例，
@@ -103,22 +110,13 @@ export const errorConfig: RequestConfig = {
   // 响应拦截器
   responseInterceptors: [
     (response) => {
-      console.log('response', response);
+      console.log('responseInterceptors', response);
       // 拦截响应数据，进行个性化处理
       // const { data } = response as unknown as ResponseStructure;
 
       // if (data?.success === false) {
       //   message.error('请求失败！');
       // }
-      if (response.status === 401) {
-        const { search, pathname } = window.location;
-        history.replace({
-          pathname: '/user/login',
-          search: stringify({
-            redirect: pathname + search,
-          }),
-        });
-      }
       return response;
     },
   ],
